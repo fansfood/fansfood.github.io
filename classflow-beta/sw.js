41616fb9d70696d926173a5d3f973dcd095fab1e
fnf010906-sys <fnf010906@gmail.com>
2026-09-17 20:00:06 +0300

    ClassFlow Beta v13: refresh service worker cache

--- classflow-beta/sw.js
+++ classflow-beta/sw.js
@@ -1,5 +1,5 @@
-const CACHE='classflow-beta-shell-v12';
-const ASSETS=['./','./index.html','./manifest.json','./local-v12.js','../classflow/icon.svg','../classflow/style.css','../classflow/layout-fix.css','../classflow/hierarchy-fix.css','../classflow/interaction-fix.css'];
+const CACHE='classflow-beta-shell-v13';
+const ASSETS=['./','./index.html','./manifest.json','./local-v13.js','../classflow/icon.svg','../classflow/style.css','../classflow/layout-fix.css','../classflow/hierarchy-fix.css','../classflow/interaction-fix.css'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('classflow-beta-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))})
