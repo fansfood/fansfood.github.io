eee651d0da5218c75efc92ca68d75e346cb3e6d9
fnf010906-sys <fnf010906@gmail.com>
2026-09-17 16:27:58 +0300

    ClassFlow Beta v9: purge experimental speech cache

--- classflow-beta/sw.js
+++ classflow-beta/sw.js
@@ -1,5 +1,5 @@
-const CACHE='classflow-beta-shell-v8';
-const ASSETS=['./','./index.html','./beta.css','./beta.js','./stable-first.js','./auth-hotfix.js','./manifest.json','../classflow/icon.svg'];
+const CACHE='classflow-beta-shell-v9';
+const ASSETS=['./','./index.html','./manifest.json','../classflow/icon.svg','../classflow/style.css','../classflow/layout-fix.css','../classflow/hierarchy-fix.css','../classflow/interaction-fix.css','../classflow/app.js','../classflow/ui-hotfix.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('classflow-beta-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))})
