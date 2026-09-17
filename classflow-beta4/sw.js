68689bb32c3a141c9cfefca0c477ae3ee011a196
fnf010906-sys <fnf010906@gmail.com>
2026-09-17 22:23:48 +0300

    Beta 4 v3: refresh cache after logo fix

--- classflow-beta4/sw.js
+++ classflow-beta4/sw.js
@@ -1,4 +1,4 @@
-const CACHE='classflow-beta4-shell-v2';
+const CACHE='classflow-beta4-shell-v3';
 const ASSETS=['./','./index.html','./beta4.css','./courses-v1.js','./manifest.json','./icon.png','../classflow-beta3/beta3.css','../classflow-beta3/app.js','../classflow-beta3/review-clean-v2.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('classflow-beta4-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
