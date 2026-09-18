74ec175f659ea727bb51ba349394e5cdad259e9f
fnf010906-sys <fnf010906@gmail.com>
2026-09-18 12:22:47 +0300

    ClassFlow Beta 1.005: refresh cache after logo fix

--- classflow-beta1005/sw.js
+++ classflow-beta1005/sw.js
@@ -1,4 +1,4 @@
-const CACHE='classflow-beta-1-005';
+const CACHE='classflow-beta-1-005-logo-fix';
 const ASSETS=['./','./index.html','./style.css','./app.js','./courses.js','./home.js','./manifest.json','./logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('classflow-beta-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
