9355761e0659daef9104006e516fb0010dbd881d
fnf010906-sys <fnf010906@gmail.com>
2026-09-18 12:03:08 +0300

    ClassFlow Beta 1.005: add sw.js

---
+++ classflow-beta1005/sw.js
@@ -0,0 +1,5 @@
+const CACHE='classflow-beta-1-005';
+const ASSETS=['./','./index.html','./style.css','./app.js','./courses.js','./home.js','./manifest.json','./logo.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('classflow-beta-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
