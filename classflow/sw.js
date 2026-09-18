f55c6d713657e39c2f6c13ad5811836a9e4d4c31
fnf010906-sys <fnf010906@gmail.com>
2026-09-18 11:10:51 +0300

    ClassFlow 4.0 v3.1: refresh service worker cache

--- classflow/sw.js
+++ classflow/sw.js
@@ -1,4 +1,4 @@
-const CACHE='classflow-shell-v4-0-3';
+const CACHE='classflow-shell-v4-0-3-1';
 const ASSETS=['./','./index.html','./base.css','./courses.css','./app.js','./review-clean-v2.js','./courses-v1.js','./stable-shell.js','./manifest.json','./icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('classflow-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
